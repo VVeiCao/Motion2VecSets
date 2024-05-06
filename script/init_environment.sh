33f05c86974394fc53d828e3f42e5ed391722457
Chang Luo <chang.luo@tum.de>
2024-05-06 11:52:10 +0200

    Update init_environment.sh

--- script/init_environment.sh
+++ script/init_environment.sh
@@ -7,7 +7,8 @@ conda env remove -n motion2vecsets -y
 
 conda create -n motion2vecsets python=3.7 -y
 conda activate motion2vecsets 
-conda install -y pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit==11.3 -c pytorch 
+conda install -y cudatoolkit=11.3 -c nvidia
+conda install -y pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 -c pytorch 
 
 pip install https://data.pyg.org/whl/torch-1.11.0%2Bcu113/torch_cluster-1.6.0-cp37-cp37m-linux_x86_64.whl
 pip install -r script/requirements.txt
